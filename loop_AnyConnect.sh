594ba49df6ac50e46250c0140253bfc1fb64c61e
Dennis Periquet <dperique@gmail.com>
2017-05-06 16:50:25 -0400

    Script to connect AnyConnect every 5 hours

---
+++ loop_AnyConnect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+# AnyConnect is useful and needed.  However, after some number of hours, it
+# disconnects.  This means you don't get mail, etc. and you have to connectd
+# again.  Any if you use the GUI app, you have to mouse it, etc.
+#
+# My solution is to just disconnect it and connect it in a predictable way
+# so that it will always be available except for that small window where we
+# disconnect it.  This was the simplest solution as I didn't feel like adding
+# logica to do checks so we can re-connect only if disconnected.
+#
+while [ 1 ] ; do
+  /opt/cisco/anyconnect/bin/vpn disconnect
+  /opt/cisco/anyconnect/bin/vpn connect AMERICA
+  sleep 30
+done
