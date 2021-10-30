8cf0c46960993a2481cdbb74e92c95116f4af60a
Dennis Periquet <dperique@gmail.com>
2021-10-30 09:27:44 -0400

    quote the message

--- whiptail_examples/scripts/continue.sh
+++ whiptail_examples/scripts/continue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-msg="This script is designed to deploy the yada project.\nPress ENTER to continue."
-whiptail --msgbox $msg  10 50
+msg="This script will deploy the yada project.\n\nPress ENTER to continue."
+whiptail --msgbox "$msg"  10 50
 echo "Continuing with deployment ..."
